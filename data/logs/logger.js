b4ca01eee46bc451a7e7db7beeaf72a07695b8c2
KOSASIH <kosasihg88@gmail.com>
2024-10-08 10:51:34 +0700

    Create logger.js

---
+++ data/logs/logger.js
@@ -0,0 +1,16 @@
+// logger.js
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+export default logger;

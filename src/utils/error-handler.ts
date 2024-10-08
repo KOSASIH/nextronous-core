1f28a87196fe6cea1597c97d3f0b226d22c99b4e
KOSASIH <kosasihg88@gmail.com>
2024-10-08 11:18:56 +0700

    Create error-handler.ts

---
+++ src/utils/error-handler.ts
@@ -0,0 +1,10 @@
+// error-handler.ts
+import { ErrorHandler } from './error-handler';
+
+class ErrorHandler {
+  async handleError(error: Error) {
+    // Handle error
+  }
+}
+
+export default ErrorHandler;
